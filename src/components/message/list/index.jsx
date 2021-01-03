5970080b8304ae7839b39c927bc760055881b4e3
yuto-oshima <oshima1998@gmail.com>
2021-01-03 23:54:36 +0900

    fetch messages

--- src/components/message/list/index.jsx
+++ src/components/message/list/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import { messagesRef } from '../../../firebase'
 
 const useStyles = makeStyles({
   root: {
@@ -9,7 +10,22 @@ const useStyles = makeStyles({
 
 const List = ({}) => {
  
+  const [messages, setMessages] = useState([])
   const classes = useStyles()
+  
+  useEffect(() => {
+    messagesRef.orderByKey().limitToLast(3).on('value', snapshot => {
+      const messages = snapshot.val()
+      if(messages === null) return
+      const entries = Object.entries(messages)
+      const newMessages = entries.map(entry => {
+        const [key, nameAndText] = entry
+        return { key, ...nameAndText }
+      })
+      setMessages(newMessages)
+    })
+  }, []);
+
   return(
     <div className={ classes.root }>Message List</div>
   )
