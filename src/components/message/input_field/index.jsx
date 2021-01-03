6f978fab083af7158c8d8a22345ea0c64f76dbd1
yuto-oshima <oshima1998@gmail.com>
2021-01-03 18:10:05 +0900

    generate avatar

--- src/components/message/input_field/index.jsx
+++ src/components/message/input_field/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Avatar ,Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
+import { gravatarPath } from '../../gravatar'
+
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
@@ -9,15 +11,16 @@ const useStyles = makeStyles({
   },
 })
 
-const InputField = props => {
+const InputField = ({ name }) => {
 
   const classes = useStyles()
+  const avatarPath = gravatarPath(name)
 
   return(
     <div className={ classes.root }>
       <Grid container>
         <Grid item={ true } xs={ 1 }>
-          <Avatar />
+          <Avatar src={ avatarPath }/>
         </Grid>
         <Grid item={ true } xs={ 10 }>入力</Grid>
         <Grid item={ true } xs={ 1 }>ボタン</Grid>
