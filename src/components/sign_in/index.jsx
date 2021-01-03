f62e08dce28e4bd9e2a9fc02e21079513e57e00d
yuto-oshima <oshima1998@gmail.com>
2021-01-03 16:45:32 +0900

    set name by button click

--- src/components/sign_in/index.jsx
+++ src/components/sign_in/index.jsx
@@ -75,12 +75,13 @@ const SignIn = ({ setName }) => {
             onChange={ e => setString(e.target.value) }
           />
           <Button
-            type="submit"
+            type="button"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
             disabled={ disabled }
+            onClick={ () => setName(string) }
           >
             はじめる
           </Button>
