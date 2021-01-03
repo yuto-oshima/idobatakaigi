3d86a933bcc0b684adf28acf8552b8d323a4eabf
yuto-oshima <oshima1998@gmail.com>
2021-01-03 17:18:07 +0900

    create config

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -4,14 +4,17 @@ import React, { Fragment, useState } from 'react'
 import Main from '../main/index.jsx'
 import SignIn  from '../sign_in/index.jsx'
 
+// Config
+import config from '../../config.json'
+
 const App = props => {
  
   const [name, setName] = useState('')
  
-  if(name) {
-    return(<Main name={ name }/>)
-  } else {
+  if(config.signInEnabled && name === '') {
     return(<SignIn setName={ setName }/>)
+  } else {
+    return(<Main name={ name }/>)
   }
 }
 
